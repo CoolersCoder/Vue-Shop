5b9c7780cbbd422c156bcbbc5cb9a2ac17f06ec0
Hu, Rui <coolerscoder@github.com>
2019-11-16 22:19:52 -0600

    Added login center box

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-
-  app root component
+  <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Login from '../components/Login.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/login', component: Login
+  },
+  {
+    path: '/', redirect: '/login'
+  }
 ]
 
 const router = new VueRouter({
